02698ca21c03138d4089cff4db57f3f83ea08726
Tejas <teanth98@gmail.com>
2021-05-07 02:13:11 +0530

    restructuring files

--- resources/views/pages/about.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-@section('content')
-        <title>{{config('app.name','DiscoverMe')}}</title>
-        <br>
-        <br>
-        <body>This is the about page</body>
-@endsection
